3e6188c68139500e5cec4f28e16c25f6de8d2118
unknown <hizaaliza707@gmail.com>
2023-02-03 10:32:29 +0700

    my01-ProjectsBirthdayReminder

---
+++ final/src/data.js
@@ -0,0 +1,38 @@
+//nama-nama yg akan ditampilkan
+export default [
+  {
+    id: 1,
+    name: 'Lee Jeno',
+    age: 23,
+    image:
+      'https://i.pinimg.com/originals/15/56/f4/1556f4d1275a9508078125956a962c97.jpg',
+  },
+  {
+    id: 2,
+    name: 'Na Jaemin',
+    age: 23,
+    image:
+      'https://i.pinimg.com/originals/97/77/4b/97774b381943e06f38abced86a009fef.jpg',
+  },
+  {
+    id: 3,
+    name: 'Johnny Suh',
+    age: 27,
+    image:
+      'https://th.bing.com/th/id/OIP.yfZXbJXsFIFb7_p-d8oNogHaNK?pid=ImgDet&rs=1',
+  },
+  {
+    id: 4,
+    name: 'Nakamoto Yuta',
+    age: 26,
+    image:
+      'https://i.pinimg.com/originals/c3/b7/26/c3b7262e42493e538df0db07eb063b27.jpg',
+  },
+  {
+    id: 5,
+    name: 'Christian Yu',
+    age: 30,
+    image:
+      'https://i.pinimg.com/736x/0a/85/75/0a8575a55a0618bc6a0bafc1e036a2e8.jpg',
+  },
+];
